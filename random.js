a961d5b59e54f11e3875225ec5ec8c56a3b40d4e
karanvazirani <105892451+karanvazirani@users.noreply.github.com>
2022-06-08 20:37:56 -0700

    Create random.js

---
+++ random.js
@@ -0,0 +1,38 @@
+/*
+    CIT 281 Project 2
+    Name: Karan Vazirani
+*/
+
+// Returns a random number between min (inclusive) and max (exclusive)
+function getRandomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+// Returns a single lowercase letter
+function getRandomLetter() {
+  
+    return alphabet[Math.floor(Math.random() * alphabet.length)]
+  }
+//Returns a random string
+function getRandomString(min, max) {
+    
+
+    return Math.floor(Math.random() * (max - min) + min);
+}
+//return a string in ascending orderq
+function  getSortedString(string){
+    var arr = str.split('');
+    var sorted = arr.sort();
+    return sorted.join('');
+  }
+
+const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".split("");
+
+let result = "";
+
+let lengthOfOutputString = getRandomInteger(10, 20);
+
+for (let i = 0; i < lengthOfOutputString; i++) {
+    result += alphabet[getRandomString(0,alphabet.length)];
+}
+
+console.log(result);
